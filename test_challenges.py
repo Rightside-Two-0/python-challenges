8b34aae081cb1c1effe85bf912adf846b0401eac
Philip Caputo Two.0 <philip@two-0.org>
2020-08-28 18:56:18 -0700

    typecheck & tests

--- test_challenges.py
+++ test_challenges.py
@@ -1,11 +1,6 @@
 #Rightside Two.0: None.profit
 #There is a rightside to money, business & property
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
-from middle_letter import middle_letter
-def test_middle_lettter():
-    assert middle_letter('abc') == 'b'
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
 from capital_indexes import capital_indexes
 def test_capital_indexes():
     assert capital_indexes('HeLLo World!') == [0,2,3,6]
@@ -22,6 +17,14 @@ def test_online_status():
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
 from randomness import random_number
+import pytest
+# @pytest.mark.repeat(3000)
 def test_random():
     number = random_number()
     assert number >= 1 and number <= 101
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+from typecheck import only_ints
+def test_typecheck():
+    bool = only_ints(1,2)
+    assert bool == True
--- typecheck.py
+++ typecheck.py
@@ -1,2 +1,12 @@
+#Rightside Two.0: None_profit
+#There is a rightside to money, business & property!
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
 #Write a function named only_ints that takes two parameters. Your function should return True if both parameters are integers, and False otherwise.
 #For example, calling only_ints(1, 2) should return True, while calling only_ints("a", 1) should return False.
+
+def only_ints(num1,num2):
+    if type(num1) == type(int(1)):
+        if type(num2) == type(int(2)):
+            return True
+        return False
+    return False
