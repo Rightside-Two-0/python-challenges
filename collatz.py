deeb166a63970a0de143f94f25fb42525778b5ef
Philip Caputo Two.0 <philip@two-0.org>
2020-08-30 13:55:16 -0700

    collatz function

--- collatz.py
+++ collatz.py
@@ -3,7 +3,7 @@
 # @Email:  philip@two-0.org
 # @Project: Python Challenge
 # @Last modified by:   two_0
-# @Last modified time: 29-08-2020
+# @Last modified time: 30-08-2020
 # @License: https://github.com/Rightside-Two-0/Rightside_Two.0/blob/master/LICENSE
 # @Copyright: Copyright 2020 © - All Rights Reserved
 '''
@@ -11,21 +11,25 @@ Collatz conjecture states that all numbers return to 1 after the
 repeated rules; if x is odd: 3x+1 else x/2
 '''
 def collatz(number):
-    steps = 0
     range_numbers = []
-    while number != 1:
+    steps = 0
+    while number > 1:
         if number % 2 == 0:
-            number = number / 2
+            number = number/2
+            range_numbers.append(number)
             steps += 1
-            range_numbers.append(number)
         else:
-            #must be odd
-            number = number*3+1
-            steps +=1
+            number = 3*number+1
             range_numbers.append(number)
+            steps += 1
     return range_numbers, steps
-y = range(0, collatz(300)[1])
-x = collatz(300)[0]
-from matplotlib import pyplot as plt
-plt.plot(y, x)
-plt.show()
+#~~~~Rightside~Two~~~~~~~~~~~~~~~~~>
+import matplotlib.pyplot as plt
+for index in range(3, 34):
+    fig = plt.figure()
+    x = range(collatz(index)[1])
+    y = collatz(index)[0]
+    plt.title('Collatz Conjecture of ('+str(index)+')')
+    plt.ylabel('height of x')
+    plt.xlabel('# of steps needed to return to source')
+    plt.plot(x,y)
