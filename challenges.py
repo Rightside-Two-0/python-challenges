e4391fb86de5134aa8326ad97449f6f28f94660a
Philip Caputo Two.0 <philip@two-0.org>
2020-09-03 06:19:06 -0700

    save file to blockchain

--- challenges.py
+++ challenges.py
@@ -1,29 +1,25 @@
 # @Author: two_0
 # @Date:   01-09-2020
 # @Email:  philip@two-0.org
-# @Project: Python Challenge
+# @Project:
 # @Last modified by:   two_0
-# @Last modified time: 02-09-2020
+# @Last modified time: 03-09-2020
 # @License: https://github.com/Rightside-Two-0/Rightside_Two.0/blob/master/LICENSE
-# @Copyright: Copyright 2020 © - All Rights Reserved
+# @Copyright: Rightside Two.0 ~ Copyright 2020 © - All Rights Reserved
+#     ___ __ ._`.*.'_._ ____ רףאל
+#    . +  * .\   o.* `.`. +.  א .
+#   *  .ת' '  \^/|  `. * .  * `
+#             \V/ . +
+#    יהוה      /_\  .`.
+#   ======== _/W\_ =אדני:By: Two.0:.*
 def personal_independence(income, ave_expenses):
     if type(income) == type('PASSIVE'):
-        return income >= ave_expenses
-    return False
+        return income > ave_expenses
 #~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
 '''
-Define a function named up_down that takes a single number as its parameter.
-Your function return a tuple containing two numbers; the first should be one
-lower than the parameter, and the second should be one higher.
+1: A function that takes a file input to be saved on blockchain
+
 '''
-def up_down(number):
-    return number-1, number+1
-#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
-'''
-Define a function named consecutive_zeros that takes a single parameter,
-which is the string of zeros and ones.
-Your function should return the number described above.
-'''
-def consecutive_zeros(input):
-    for item in input:
-        if item 
+def save_file(filename):
+    with open(filename, mode='r') as file:
+        content = file.readlines()
