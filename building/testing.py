9b263501f50bb4fdbda6399a39dc14e162926372
Philip Caputo Two.0 <philip@two-0.org>
2020-09-03 14:46:38 -0700

    updating

--- building/testing.py
+++ building/testing.py
@@ -1,11 +1,11 @@
 # @Author: two_0
-# @Date:   01-09-2020
+# @Date:   03-09-2020
 # @Email:  philip@two-0.org
-# @Project:
+# @Project: Python Challenge
 # @Last modified by:   two_0
 # @Last modified time: 03-09-2020
 # @License: https://github.com/Rightside-Two-0/Rightside_Two.0/blob/master/LICENSE
-# @Copyright: Rightside Two.0 ~ Copyright 2020 © - All Rights Reserved
+# @Copyright: Copyright 2020 © - All Rights Reserved
 #     ___ __ ._`.*.'_._ ____ רףאל
 #    . +  * .\   o.* `.`. +.  א .
 #   *  .ת' '  \^/|  `. * .  * `
@@ -16,8 +16,3 @@ def personal_independence(income, ave_expenses):
     if type(income) == type('PASSIVE'):
         return income > ave_expenses
 #~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
-'''
-Create a function that returns 2 strings printed out.
-1st: represents the problem (addition) --> **** + **
-2nd: represents the answer the characters & the # --> ****** 6
-'''
