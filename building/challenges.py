c374ca870f14b14cc57d105ff422773c496868d1
Philip Caputo Two.0 <philip@two-0.org>
2020-09-14 17:53:58 -0700

    updating

--- building/challenges.py
+++ building/challenges.py
@@ -4,7 +4,7 @@
 # @Email:  philip@two-0.org
 # @Project: Python Challenge
 # @Last modified by:   two_0
-# @Last modified time: 13-09-2020
+# @Last modified time: 14-09-2020
 # @License: https://github.com/Rightside-Two-0/Rightside_Two.0/blob/master/LICENSE
 # @Copyright: Rightside Two.0 ~ Copyright 2020 © - All Rights Reserved
 #     ___ __ ._`.*.'_._ ____ רףאל
@@ -17,24 +17,24 @@ def personal_independence(income, ave_expenses):
     if type(income) == type('PASSIVE'):
         return income > ave_expenses
 #~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
-#%% markdown
+#%% md
 '''
 Create a function that returns 2 strings printed out.
 1st: represents the problem (addition) --> **** + **
 2nd: represents the answer the characters & the # --> ****** 6
 '''
-#%% codecell
+#%% 
 def add_it(number1, number2):
     print('*'*number1,'+','*'*number2)
     print('*'*(number1+number2), number1+number2)
     return number1+number2
 #~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
-#%% markdown
+#%% md
 '''
 Define a function named consecutive_zeros that takes a single parameter,
 which is the string of zeros and ones.
 '''
-#%% codecell
+#%%
 def consecutive_zeros(input):
     count = 1
     longest = 0
@@ -51,12 +51,12 @@ def consecutive_zeros(input):
                 count = 1
     return longest
 #~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
-#%% markdown
+#%% md
 '''
 Define a function named all_equal that takes a list and checks whether
 all elements in the list are the same.
 '''
-#%% codecell
+#%%
 def all_equal(input):
     is_true = True
     for index in range(len(input)):
@@ -65,23 +65,23 @@ def all_equal(input):
     return is_true
 
 #~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
-#%% markdown
+#%% md
 '''
 Define a function named triple_and that takes three parameters and
 returns True only if they are all True and False otherwise.
 '''
-#%% codecell
+#%%
 def triple_and(input1, input2, input3):
     return input1 and input2 and input3
 triple_and(True, False, True)
 
 #~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
-#%% markdown
+#%% md
 '''
 Define a function named convert that takes a list of numbers as its only
 parameter and returns a list of each number converted to a string.
 '''
-#%% codecell
+#%%
 def convert(input):
     return [str(x) for x in input]
 
---
+++ building/linear_algebra.py
@@ -0,0 +1,14 @@
+# @Author: two_0
+# @Date:   14-09-2020
+# @Email:  philip@two-0.org
+# @Project: Python Challenge
+# @Last modified by:   two_0
+# @Last modified time: 14-09-2020
+# @License: https://github.com/Rightside-Two-0/Rightside_Two.0/blob/master/LICENSE
+# @Copyright: Copyright 2020 © - All Rights Reserved
+import numpy as np
+from IPython.display import Latex
+Latex('''
+    $$ y_{1} = a*x_{1} + b*x_{2}$$
+    $$ y_{2} = c*x_{1} + d*x_{2} $$
+''')
