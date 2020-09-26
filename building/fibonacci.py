f49470a70774ecb577dab93a50df17ed7b15e6aa
Philip Caputo Two.0 <philip@two-0.org>
2020-09-26 06:58:00 -0700

    fibonacci series

--- building/fibonacci.py
+++ building/fibonacci.py
@@ -19,3 +19,22 @@ def personal_independence(income, ave_expenses):
 ##%%md
 #Fibonacci Numbers
 #%%
+from IPython.display import *
+IFrame('https://en.wikipedia.org/wiki/Fibonacci', width=600, height=400)
+#%%
+YouTubeVideo('wTlw7fNcO-0', start=10)
+#%%
+def fibonacci_series(n_terms):
+    n_1 = 0
+    n_2 = 1
+    count = 0
+    print(n_2)
+    while count < n_terms:
+        fib_sum = n_1  + n_2
+        n_1 = n_2
+        n_2 = fib_sum
+        count += 1
+        print(fib_sum)
+#%%
+#testing
+fibonacci_series(10)
